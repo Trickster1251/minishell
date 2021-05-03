217d9b24e805b2e22ce76c6e51cd4b4a4681f9ef
Aeneas Mustafar <aemustaf@et-c6.kzn.21-school.ru>
2021-05-04 00:05:22 +0300

    gnl fix by a norm

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -16,12 +16,13 @@
 
 static char	*check_remainder(char **remainder, char **line)
 {
-	char *p_new;
+	char	*p_new;
 
 	p_new = NULL;
 	if (*remainder)
 	{
-		if ((p_new = my_strchr(*remainder, '\n')))
+		p_new = my_strchr(*remainder, '\n');
+		if (p_new)
 		{
 			*p_new = '\0';
 			p_new++;
@@ -49,31 +50,46 @@ static int	errors(char *buf)
 	return (-1);
 }
 
-int			get_next_line(int fd, char **line)
+static int	ft_read_line(int fd, char **line, char **remainder, t_gnl *g)
 {
-	char		*buf;
-	char		*p_new;
-	int			readed_byte;
+	g->r_b = read(fd, g->buf, BUFFER_SIZE);
+	while (!g->p_new && g->r_b)
+	{
+		g->buf[g->r_b] = '\0';
+		g->p_new = my_strchr(g->buf, '\n');
+		if (g->p_new)
+		{
+			*g->p_new = '\0';
+			*remainder = my_strdup(++g->p_new);
+			if (!*(remainder))
+				return (errors(g->buf));
+		}
+		*line = my_strjoin(*line, g->buf);
+		if (!(*line))
+			return (errors(g->buf));
+		g->r_b = read(fd, g->buf, BUFFER_SIZE);
+	}
+	return (g->r_b);
+}
+
+int	get_next_line(int fd, char **line)
+{
+	t_gnl		g;
 	static char	*remainder;
 
-	buf = NULL;
-	if (fd < 0 || !line || BUFFER_SIZE <= 0 || (read(fd, buf, 0) == -1))
+	g.buf = NULL;
+	if (fd < 0 || !line || BUFFER_SIZE <= 0 || (read(fd, g.buf, 0) == -1))
 		return (-1);
-	if (!(buf = (char*)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+	g.buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!g.buf)
 		return (-1);
-	p_new = check_remainder(&remainder, line);
-	while (!p_new && (readed_byte = read(fd, buf, BUFFER_SIZE)))
-	{
-		buf[readed_byte] = '\0';
-		if ((p_new = my_strchr(buf, '\n')))
-		{
-			*p_new = '\0';
-			if (!(remainder = my_strdup(++p_new)))
-				return (errors(buf));
-		}
-		if (!(*line = my_strjoin(*line, buf)))
-			return (errors(buf));
-	}
-	free(buf);
-	return ((p_new || readed_byte) ? 1 : 0);
+	g.p_new = check_remainder(&remainder, line);
+	g.r_b = ft_read_line(fd, line, &remainder, &g);
+	if (g.r_b < 0)
+		return (-1);
+	free(g.buf);
+	if (g.p_new || g.r_b)
+		return (1);
+	else
+		return (0);
 }
